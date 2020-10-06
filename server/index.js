6aaba89ed7627012c91fd9d65b4fd01d800b7de8
connor-ashlock <connor9804@yahoo.com>
2020-10-06 02:43:28 +0000

    added get endpoint that retrieves all restaurants with a corresponding categoryId

--- server/index.js
+++ server/index.js
@@ -14,8 +14,28 @@ app.use(sessionMiddleware);
 app.use(express.json());
 
 app.get('/api/health-check', (req, res, next) => {
-  db.query(`select 'successfully connected' as "message"`)
+  db.query('select \'successfully connected\' as "message"')
     .then(result => res.json(result.rows[0]))
+    .catch(err => next(err));
+});
+
+app.get('/api/category/:categoryId', (req, res, next) => {
+  const id = parseInt(req.params.categoryId, 10);
+  if (!id || id < 1) {
+    return next(new ClientError('The categoryId must be a positive integer', 400));
+  }
+  const selectRestaurants = `
+    select *
+      from "restaurants"
+     where "categoryId" = $1;
+  `;
+  db.query(selectRestaurants, [id])
+    .then(result => {
+      if (result.rows.length === 0) {
+        return next(new ClientError('The categoryId cannot be found', 404));
+      }
+      return res.json(result.rows);
+    })
     .catch(err => next(err));
 });
 
