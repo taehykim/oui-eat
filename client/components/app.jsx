df48e8eeda6fd541ca3759db4d9bac4bfd9982a8
connor-ashlock <connor9804@yahoo.com>
2020-10-08 00:17:09 +0000

    adjusted app view to be set to home again

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -12,7 +12,7 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       view: {
-        name: 'account',
+        name: 'home',
         params: {},
         currentCategory: null
       },
