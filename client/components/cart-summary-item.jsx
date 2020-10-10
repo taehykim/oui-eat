f54f7b63c85cce52016a59bb6e7a1135a2c0be1d
Tia <tiarekim@gmail.com>
2020-10-10 00:30:08 +0000

    user can remove an item from their cart front end

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -1,13 +1,55 @@
 import React from 'react';
 
 class CartSummaryItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onRemoveClick = this.onRemoveClick.bind(this);
+    this.getCartId = this.getCartId.bind(this);
+  }
+
+  onRemoveClick(event) {
+    const cartItemId = this.getCartId();
+
+    fetch(`/api/cart/${cartItemId}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then()
+      .then(data => {
+        this.props.updateCart();
+      })
+      .catch(err => console.error(err));
+  }
+
+  getCartId() {
+    const keys = Object.keys(this.props.cartItems);
+
+    for (let i = 0; i < keys.length; i++) {
+      if (this.props.cartItems[i].name === this.props.item.name) {
+        return this.props.cartItems[i].cartItemId;
+      }
+    }
+    return null;
+  }
+
   render() {
     return (
-      <div className="d-flex align-items-center">
-        <span className="badge badge-light mr-1 h5">
-          {this.props.item.count}
-        </span>
-        <p className="h5 font-weight-light">{this.props.item.name}</p>
+      <div className="d-flex align-items-center justify-content-between mb-1">
+        <div className="d-flex align-items-center">
+          <span className="badge badge-light mr-1 h5">
+            {this.props.item.count}
+          </span>
+          <p className="h5 font-weight-light">{this.props.item.name}</p>
+        </div>
+        <div>
+          <button
+            type="button"
+            className="btn btn-danger px-2 py-1"
+            onClick={this.onRemoveClick}
+          >
+            Remove
+          </button>
+        </div>
       </div>
     );
   }
