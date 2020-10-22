15efb578146ad09d5e48190eccb0f66d40caf64c
connor-ashlock <connor9804@yahoo.com>
2020-10-22 22:56:22 +0000

    Added disclaimer to app

--- client/components/disclaimer-modal.jsx
+++ client/components/disclaimer-modal.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function DisclaimerModal(props) {
   function handleClick() {
-    props.setView('catalog', {});
+    props.setView('home', {});
   }
 
   return (
-    <div className="row justify-content-center">
+    <div className="row justify-content-center align-items-center mt-5">
       <div className="d-flex flex-wrap justify-content-center col-9 rounded p-4 modal1">
         <h2 className="col-12 d-flex justify-content-center">Disclaimer</h2>
         <p className="col-12">This website is just a demo and no real purchases will be made. Please do not use any personal information. Please click the button below to continue. Thank you and enjoy!</p>
